c7a5ee155b39aa3247c92498d0fdc045a28d20fe
Isabella Caballero <susanamoreno9498@gmail.com>
2020-07-09 14:59:07 -0500

    Metodo para determinar aprobacion

---
+++ aprobado.py
@@ -0,0 +1,9 @@
+
+#Autor: Isabella Caballero  Moreno
+#Ejercicio que permita 
+def determinar_aprobado(promedio):
+    if promedio>=11:
+        resultado="Aprobado"
+    else:
+        resultado="Desaprobado"
+    return resultado
