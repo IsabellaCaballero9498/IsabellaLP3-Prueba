6c7eb7b06fc03ad358721fb8480d3f9c678eb153
Isabella Caballero <susanamoreno9498@gmail.com>
2020-07-09 15:09:40 -0500

    Ejercicio para determinar aprobado-listo

--- aprobado.py
+++ aprobado.py
@@ -2,8 +2,12 @@
 #Autor: Isabella Caballero  Moreno
 #Ejercicio que permita 
 def determinar_aprobado(promedio):
+  
     if promedio>=11:
         resultado="Aprobado"
     else:
         resultado="Desaprobado"
     return resultado
+
+promedio=int(input("Promedio: "))
+print(determinar_aprobado(promedio))
